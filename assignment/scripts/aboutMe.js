d1297723928546426e60bc3d5eb0e7ab40ab800c
SRJ-91 <Sawyerjen@hotmail.com>
2023-03-30 11:25:27 -0500

    Update aboutMe.js
    final commit

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -80,6 +80,33 @@ console.log(mostPets);
 //      Otherwise, log "Luck is what happens when preparation meets opportunity"
 //      You'll need to research how to use switch statements!
 
+switch (luckyNumber) {
+    case 1:
+        console.log("First is the worst");
+        break;
+    case 2:
+        console.log("Second is the best");
+        break;
+    case 3:
+        console.log("Third is the one with the polka dot dress")
+        break;
+    default:
+        console.log("Luck is what happens when preparation meets opportunity")
+}
+
+
 // 18 -- Rewrite question 13 with a `ternary` operator. You'll need to do research!
+
+/*if (adventurous === true) {
+    console.log("Adventures are great!");
+} else {
+    console.log("How about we stay home?");
+}*/
+
+
+
+let adventurousCheck = (adventurous === true) ? "Adventures are great!" : "How about we stay home?";
+console.log(adventurousCheck);
+
 
 
